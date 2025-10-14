a85d52d9931c3a66f7fef11b26974a1d8b544ab4
Lia Marian Cruz Gaytán <lia.cruz@infotec.mx>
2025-10-14 10:25:23 -0600

    settings config

--- proyecto_1/settings.py
+++ proyecto_1/settings.py
@@ -121,23 +121,20 @@ CSRF_COOKIE_SECURE = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
-
 import dj_database_url
 
-if not DEBUG:
+if config('DATABASE_URL', default=None):
     DATABASES = {
         'default': dj_database_url.config(default=config('DATABASE_URL'))
     }
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
+    }
